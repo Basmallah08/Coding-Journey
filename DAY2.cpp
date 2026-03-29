cfead1fb596471abc9041e24f1469f8f1219115e
Basmallah08 <bosymontaser10@gmail.com>
2026-03-29 23:13:54 +0200

    Add files via upload

---
+++ DAY2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Variables definition
+    string studentName = "Basmala";
+    int dayNumber = 2;
+    string goal = "Learning Variables";
+
+    cout << "Student: " << studentName << endl;
+    cout << "Current Progress: Day " << dayNumber << " of 30" << endl;
+    cout << "Goal: " << goal << endl;
+
+    return 0;
+}
